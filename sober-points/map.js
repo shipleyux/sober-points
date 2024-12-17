1ba29fb95c39879df6cc66968ef892b3b81926e9
Sara Shipley <sarashipley1@gmail.com>
2024-12-17 15:41:42 +0000

    setting up map

--- sober-points/map.js
+++ sober-points/map.js
@@ -1,1 +1,11 @@
-document.getElementById("map").innerHTML = "Good Bye World";
+
+
+import * as L from "leaflet";
+
+// initialize the map
+const map = L.map ("map").setView([51.505,-0.09], 13)
+
+L.tileLayer ("https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright"' 
+).addTo(map);
